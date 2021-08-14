2b037cf8e2ce110b86d3850017e8a55cbf55e9f5
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2021-08-14 13:22:50 -0400

    todos to done

--- script.js
+++ script.js
@@ -6,11 +6,24 @@ var todosList = document.getElementById('todos-list')
 
 var todos= ["test", "testsdlf;jas;d", "walk the dog"]
 
+//Loop through todos
+for (var i=0; i<todos.length;i++) {
+
+//create a LI
+var li = document.createElement('li')
+//set the innerText or textcontent
+li.innerText = todos[i]
+//append into DOM
+todosList.appendChild(li)
+}
+
+
 function addTodo(event){
     event.preventDefault()
     var newTodoText = newTodoInput.value
     todos.push(newTodoText)
     newTodoInput.value = ''
+
 }
 
 
