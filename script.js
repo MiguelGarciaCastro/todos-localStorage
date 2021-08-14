3432be19871269f172c60cb99ff4fc9dd92339ab
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2021-08-14 13:51:18 -0400

    save/pull from localStroage

--- script.js
+++ script.js
@@ -4,28 +4,30 @@ var newTodoInput = document.getElementById('new-todo')
 var todosList = document.getElementById('todos-list')
 
 
-var todos= ["test", "testsdlf;jas;d", "walk the dog"]
-
-//Loop through todos
-for (var i=0; i<todos.length;i++) {
-
-//create a LI
-var li = document.createElement('li')
-//set the innerText or textcontent
-li.innerText = todos[i]
-//append into DOM
-todosList.appendChild(li)
+var todos= JSON.parse(localStorage.getItem('todos'))
+console.log(todos)
+function renderTodos() {
+    todosList.innerHTML = ''
+    for (var i = 0; i<todos.length;i++) {
+    //create a LI
+    var li = document.createElement('li')
+    //set the innerText or textcontent
+    li.innerText = todos[i]
+    //append into DOM
+    todosList.appendChild(li)
+    }
 }
-
 
 function addTodo(event){
     event.preventDefault()
     var newTodoText = newTodoInput.value
     todos.push(newTodoText)
+    localStorage.setItem('todos', JSON.stringify(todos))
     newTodoInput.value = ''
-
+    renderTodos()
 }
 
 
 
 newTodoForm.addEventListener('submit', addTodo)
+renderTodos()
